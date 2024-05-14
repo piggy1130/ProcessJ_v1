b1a3833072661b968ba50f8c6c158d78179eb9c9
CodingPiggy <52862812+piggy1130@users.noreply.github.com>
2024-05-14 11:34:23 -0700

    no serial & no delay()

--- device_simulation.ino
+++ device_simulation.ino
@@ -5,7 +5,7 @@ int device_state = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); // serial communication at 9600 bits per second
+  // Serial.begin(9600); // serial communication at 9600 bits per second
   pinMode(start_device_pin, INPUT); 
   pinMode(LED_BUILTIN, OUTPUT);
   //pinMode(end_device_pin, INPUT);
@@ -14,14 +14,14 @@ void setup() {
 void loop() {
   
   if (digitalRead(start_device_pin) == HIGH){
-    Serial.println("Device ON");
+    //Serial.println("Device ON");
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000); // delay 1s
+    //delay(1000); // delay 1s
   }
   if (digitalRead(start_device_pin) == LOW){
-    Serial.println("Device OFF");
+    //Serial.println("Device OFF");
     digitalWrite(LED_BUILTIN, LOW);
-    delay(1000); // delay 1s
+    //delay(1000); // delay 1s
   }
 
 }
