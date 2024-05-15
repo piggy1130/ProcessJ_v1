d173296ba8b179d16d6ce39080c54e22034e2f9a
CalyrexThePal <2801789sss@gmail.com>
2024-05-15 15:19:42 -0700

    ProcessJ could read pin status now

---
+++ ProcessJ/lib/JVM/std/pi5.java
@@ -0,0 +1,54 @@
+package std;
+
+import com.pi4j.io.gpio.*;
+
+public class pi5 {
+    public static final int PIN_MAX = 40;
+    // only 21 pins could be used as GPIO
+    private static final int[] PI_GPIO = {2,3,4,17,27,22,5,6,13,19,26,
+                                        14,15,18,23,24,25,12,16,21};
+    
+    private static GpioController pi;
+    private static GpioPinDigitalOutput[] pins = new GpioPinDigitalOutput[PIN_MAX];
+    private static GpioPinDigitalInput input_pin;
+
+    public static void pi5_println(String s){
+        System.out.println("UPDATED - version 5!!! Jiaqi is testing GPIO...");
+        System.out.println(s);
+    }
+
+    private static void initializePins(){
+        pi = GpioFactory.getInstance();
+        GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING));
+
+        for (int pin : PI_GPIO){
+            System.out.println(pin);
+            Pin pin_num = RaspiPin.getPinByAddress(pin);
+            pins[pin] = pi.provisionDigitalOutputPin(pin_num, "MyOutput", PinState.LOW);
+            // Ensure the pin is shutdown when the program terminates                                                                                             
+            pins[pin].setShutdownOptions(true, PinState.LOW);
+        }
+
+        input_pin = pi.provisionDigitalInputPin(RaspiPin.GPIO_20, PinPullResistance.PULL_DOWN);
+
+    }
+
+    static{
+        initializePins();
+    }
+
+    public static void set_high_pi5(int pinNo){
+        System.out.println("on");
+        pins[pinNo].high();
+    }
+
+    public static void set_low_pi5(int pinNo){
+        System.out.println("off");
+        pins[pinNo].low();
+    }
+
+    public static void get_pin_info(){
+        System.out.println("GPIO Pin State is: " + input_pin.getState());
+    }
+
+}
--- ProcessJ/lib/JVM/std/sleep.java
+++ ProcessJ/lib/JVM/std/sleep.java
@@ -4,9 +4,9 @@ public class sleep {
     
     public static void jl_sleep(int t){
         try{
-            System.out.println("Jiaqi is testing Sleep...");
+            //System.out.println("Jiaqi is testing Sleep...");
             Thread.sleep(t);
-            System.out.println("Awake!");
+            //System.out.println("Awake!");
         } catch(InterruptedException e){
             System.err.println("Sleep was interrupted");
         }
